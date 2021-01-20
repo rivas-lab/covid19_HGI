85ae7ed4be96c5a183add5facde270b27a31cd23
Yosuke Tanigawa <ytanigaw@stanford.edu>
2021-01-20 12:27:07 -0800

    add one more page

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,11 @@ FROM python:3-slim
 # RUN apt-get -y update \
 #  && apt−get install −y pip3 build−essential
 
-COPY web/requirements.txt requirements.txt
-RUN pip3 install -r requirements.txt
-COPY web .
-COPY fig fig
+COPY web/requirements.txt /opt/requirements.txt
+RUN pip3 install -r /opt/requirements.txt
+COPY fig /web/static/covid19HGI/fig
+COPY web/templates /web/templates
+COPY web/app.py /web/app.py
+
 ENTRYPOINT ["python3"]
-CMD ["app.py"]
+CMD ["/web/app.py"]
--- web/app.py
+++ web/app.py
@@ -7,8 +7,35 @@ app = Flask(__name__)
 #Create a route decorator to tell the application, which URL should be called for the #described function and define the function
 
 @app.route('/')
-def main_page():
-    return render_template('main.html')
+@app.route('/covid19HGI')
+def covid19HGI_main():
+    return render_template(
+        'covid19HGI/main.html',
+        #=======================#
+        _HGI_v = 'HGIrel5',
+        _HGI_case_controls=['B2', 'C2', 'B1', 'A2'],
+        _HGI_suffices=['eur_leave_ukbb_23andme', 'eur_leave_23andme', 'leave_UKBB_23andme', 'leave_23andme'],
+        _clump_p1s=['1e-5', '1e-4', '1e-3'],
+        #=======================#
+        _HGI_sx = 'eur_leave_ukbb_23andme',
+        _HGI_cc = 'B2',
+        _clumpp = '1e-5'
+    )
+
+@app.route('/covid19HGI/PRS_PheWAS/<HGI_sx>/')
+def covid19HGI_prs_phewas(HGI_sx):
+    return render_template(
+        'covid19HGI/prs_phewas.html',
+        #=======================#
+        _HGI_v = 'HGIrel5',
+        _HGI_case_controls=['B2', 'C2', 'B1', 'A2'],
+        _HGI_suffices=['eur_leave_ukbb_23andme', 'eur_leave_23andme', 'leave_UKBB_23andme', 'leave_23andme'],
+        _clump_p1s=['1e-5', '1e-4', '1e-3'],
+        #=======================#
+        HGI_sx = HGI_sx,
+        _HGI_cc = 'B2',
+        _clumpp = '1e-5'
+    )
 
 #Create the main driver function
 if __name__ == '__main__':
