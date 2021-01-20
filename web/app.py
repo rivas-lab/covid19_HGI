049c54d5af7533e19527b32c254c2b0704875806
Yosuke Tanigawa <ytanigaw@stanford.edu>
2021-01-20 15:24:03 -0800

    update

--- web/app.py
+++ web/app.py
@@ -1,4 +1,5 @@
-#Import the flask module
+import os
+
 from flask import Flask, render_template
 import pandas
 
@@ -7,19 +8,16 @@ app = Flask(__name__)
 
 #Create a route decorator to tell the application, which URL should be called for the #described function and define the function
 
-@app.route('/')
-@app.route('/covid19HGI')
-def covid19HGI_main():
-    tbl='/web/static/covid19HGI/fig/HGIrel5.PRS_PheWAS.1e-5.tsv.gz'
-    df = pandas.read_csv(tbl, sep='\t')
-    GBE_top='https://gbe.stanford.edu'
-
+def covid19HGI_read_res_table(static_path='/web/static/', GBE_top='https://gbe.stanford.edu'):
+    tbl=os.path.join(static_path, 'covid19HGI/fig/HGIrel5.PRS_PheWAS.1e-5.tsv.gz')
     subset_map={
         'eur_leave_ukbb_23andme': 'EUR (-UKB)',
         'eur_leave_23andme': 'EUR',
         'leave_UKBB_23andme': 'All (-UKB)',
         'leave_23andme': 'All'
     }
+
+    df = pandas.read_csv(tbl, sep='\t')
 
     df['GBE_short_name'] = ['<a href="{}/RIVAS_HG19/coding/{}">{}</a>'.format(GBE_top, x[0], x[1]) for x in zip(df['GBE_ID'], df['GBE_short_name'])]
     df = df.drop('GBE_ID',axis=1)
@@ -34,6 +32,15 @@ def covid19HGI_main():
         df[col] = df[col].map(lambda x: '{:.0e}'.format(x).replace('-0', '-') )
     for col in ['P']:
         df[col] = df[col].map(lambda x: '{:.2e}'.format(x))
+
+    return(df)
+
+
+@app.route('/')
+@app.route('/covid19HGI')
+def covid19HGI_main():
+    df=covid19HGI_read_res_table()
+
     table_body_str=''.join(['<tr>{}</tr>'.format(
         ''.join(['<td>{}</td>'.format(x) for x in df.iloc[row]])
     ) for row in range(df.shape[0])])
@@ -71,10 +78,10 @@ def covid19HGI_prs_phewas(HGI_sx):
         _clumpp = '1e-5'
     )
 
-@app.route('/covid19HGI/PRS_PheWAS/<HGI_sx>/chr3')
-def covid19HGI_prs_phewas_chr3(HGI_sx):
+@app.route('/covid19HGI/PRS_PheWAS/<HGI_sx>/<HGI_cc>')
+def covid19HGI_prs_phewas_more(HGI_sx, HGI_cc):
     return render_template(
-        'covid19HGI/prs_phewas_chr3.html',
+        'covid19HGI/prs_phewas_more.html',
         #=======================#
         _HGI_v = 'HGIrel5',
         _HGI_case_controls=['B2', 'C2', 'B1', 'A2'],
@@ -83,10 +90,9 @@ def covid19HGI_prs_phewas_chr3(HGI_sx):
         _GBE_IDs=['INI30130', 'INI30190', 'INI30610', 'INI10030610'],
         #=======================#
         HGI_sx = HGI_sx,
-        _HGI_cc = 'B2',
+        HGI_cc = HGI_cc,
         _clumpp = '1e-5'
     )
-
 
 #Create the main driver function
 if __name__ == '__main__':
